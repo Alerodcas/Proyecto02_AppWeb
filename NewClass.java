fb2bf87e2d74093cdc77ce988c88c1981bb07ce1
Alejadra <alerodriguezc2002@gmail.com>
2021-10-06 00:40:51 +0100

    Creacion clase vacia

---
+++ NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto02.Proyecto02_AppWeb;
+
+/**
+ *
+ * @author alero
+ */
+public class NewClass {
+    
+}
